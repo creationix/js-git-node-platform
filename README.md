6afa500c2bc0f9f8d55abff7db962b4e8b762c47
Tim Caswell <tim@creationix.com>
2013-08-08 22:33:27 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+js-git-node-platform
+====================
+
+Platform for js-git to run on node.js
---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  fs: require('./fs.js'),
+  tcp: require('./tcp.js'),
+  http: require('./http.js'),
+  ssh: require('./ssh.js'),
+  sha1: require('./sha1.js'),
+  inflate: require('./inflate.js'),
+  deflate: require('./deflate.js'),
+  trace: require('./trace.js'),
+  agent: "jsgit/" + require('js-git/package.json').version,
+};
